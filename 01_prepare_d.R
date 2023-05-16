590995f25bf4152ce391a10ad08dc8d1a2532c98
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2023-05-16 17:49:30 +0200

    Create 01_prepare_d.R

---
+++ 01_prepare_d.R
@@ -0,0 +1,42 @@
+library(sf)
+library(rnaturalearth)
+library(tidyverse)
+library(raster)
+library(rgdal)
+
+DT2.oa$Species <- gsub(DT2.oa$Species,pattern = ' ',replacement = '_')
+species_level <- DT2.oa[grepl("_", DT2.oa$Species),]
+NOspecies_level <- DT2.oa[!grepl("_", DT2.oa$Species),] #tutte le osservazioni che non possono essere considerate
+
+
+#d$d.Species <- gsub(d$d.Species,pattern = ' ',replacement = '_')
+
+#Unione delle 2 tabelle necessarie per le analisi e selezione delle variabili
+d <- merge(species_level, header.oa, by='PlotObservationID')
+
+d <- d[d$Continent=='Europe',]
+d <- d %>% filter(Location_uncertainty < 250)
+d$Year <- substr(d$Date_of_recording, 1,4)
+d <- d %>% filter(Year >= 1992)
+
+#length(unique(d$PlotObservationID))
+#[1] 10501
+
+x <- raster()
+e <- extent( -180, 180, -90, 90)
+r<-raster::crop(x, e)
+r <- disaggregate(r, fact=2)
+vals <- 1:ncell(r)
+r <- setValues(r, vals)
+r
+r.sp <- as(r, "SpatialPolygonsDataFrame")
+crs(r.sp) <- "+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs "
+
+dCoord <- d%>% dplyr::select(Longitude, Latitude, PlotObservationID)
+coordinates(dCoord)= ~Longitude+Latitude
+crs(dCoord)<-  "+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs "
+OV<- over(dCoord, r.sp)
+colnames(OV) <- "id"
+d<- cbind(OV, d)
+write_rds(d,"E:/Dottorato2/sPlot/d.rds")
+
