95f27bc3cdeab3c1030d0c48e44d38fc0c330bd4
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2023-05-18 12:26:32 +0200

    Update 09_prepare_TBiasTPD.R

--- 09_prepare_TBiasTPD.R
+++ 09_prepare_TBiasTPD.R
@@ -1,25 +1,22 @@
 require(TPD)
 require(tidyverse)
 dd <- readRDS('dYear.rds')
-dd %>% 
+
+dd %>% dplyr::select(-PlotObservationID)%>%
+  unique%>%
   mutate(Year=as.numeric(Year)) %>% 
   pivot_wider(names_from = Year,values_from = nPlotY) %>% 
-  dplyr::select(id, PlotObservationID,order(colnames(.))) %>% 
+  dplyr::select(id, order(colnames(.))) %>% 
   as.data.frame() -> ddWide
 
 ddWide[is.na(ddWide)] <- 0
 
-ddWide %>% 
-  group_by(id) %>% 
-summarise_all(.funs=function(x) sum(x,na.rm=TRUE)) %>% 
-  dplyr::select(-PlotObservationID)->ddWide
-
-head(ddWide)
 
 
 
 # calcluate Pielou's evenness
 
+library(vegan)
 
 df <- data.frame(S=rowSums(ddWide[,2:24]),
                  H=diversity(ddWide[,2:24]))
