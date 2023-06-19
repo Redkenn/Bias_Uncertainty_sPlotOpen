e79fdda49ca0847ec5a988331db817529253271d
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2023-06-19 16:09:50 +0200

    Update 07_prepare_model_cor.R

--- 07_prepare_model_cor.R
+++ 07_prepare_model_cor.R
@@ -3,7 +3,6 @@ library(rnaturalearth)
 library(tidyverse)
 library(raster)
 library(rgdal)
-library(mgcv)
 library(SpatialPack)
 
 
@@ -61,87 +60,6 @@ Vpop05 <- extract(pop05, ctr)
 NAT_DIS_NNI_POP <- cbind(NAT_DIS_NNI, pop=Vpop05)%>% relocate("NNI", .before = "median_dist")
 NAT_DIS_NNI_POP <- NAT_DIS_NNI_POP[,-1]
 
-######## select and clean for GAM with comp ########
-
-cells <- NAT_DIS$id
-ctr <- as.data.frame(xyFromCell(r, cells))
-coordinates(ctr)= ~x+y
-crs(ctr)=crs(pop05)
-
-Vpop05 <- extract(pop05, ctr)
-
-NAT_DIS_POP <- cbind(NAT_DIS, pop=Vpop05)
-
-
-
-Vcompr <- extract(compr, ctr)
-
-NAT_DIS_POP_COMP <- cbind(NAT_DIS_POP, comp =Vcompr)
-NAT_DIS_POP_COMP <- NAT_DIS_POP_COMP %>% drop_na() %>% relocate("comp.Completeness", .before = "median_dist")
-NAT_DIS_POP_COMP <- NAT_DIS_POP_COMP[,-c(1,6)]
-
-############# GAM ############
-NNI <- 1
-pred_cols <- 2:4
-
-names(NAT_DIS_NNI_POP)[NNI]
-names(NAT_DIS_NNI_POP)[pred_cols]
-
-form_gamNNI <- as.formula(paste0(names(NAT_DIS_NNI_POP)[NNI], "~", paste0("s(", names(NAT_DIS_NNI_POP)[pred_cols], ")", collapse = "+")))
-ModelNNI <- gam(form_gamNNI, data = NAT_DIS_NNI_POP)
-
-#R-sq.(adj) =  0.201   Deviance explained = 21.9%
-
-COMP <- 1
-pred_cols <- 2:4
-
-names(NAT_DIS_POP_COMP)[COMP]
-names(NAT_DIS_POP_COMP)[pred_cols]
-
-form_gamCOMP <- as.formula(paste0(names(NAT_DIS_POP_COMP)[COMP], "~", paste0("s(", names(NAT_DIS_POP_COMP)[pred_cols], ")", collapse = "+")))
-ModelCOMP <- gam(form_gamCOMP, data = NAT_DIS_POP_COMP)
-
-#R-sq.(adj) =  0.352   Deviance explained = 36.3%
-
-
-
-######## RF ########
-library(ranger)
-ModelRF_NNI<-ranger(NAT_DIS_NNI_POP$NNI ~., data= NAT_DIS_NNI_POP, importance='impurity') 
-ModelRF_COMP<-ranger(NAT_DIS_POP_COMP$comp.Completeness ~., data= NAT_DIS_POP_COMP, importance='impurity')
-
-# ModelRF_COMP[["r.squared"]]
-#[1] 0.3094125
-
-#ModelRF_NNI[["r.squared"]]
-#[1] 0.2005293
-
-
-################ PEARSON'S CORRELATION ###############
-
-NAT_DIS_NNI_POP <- cbind(NAT_DIS_NNI, pop=Vpop05)%>% relocate("NNI", .before = "median_dist")
-
-ctr <- as.data.frame(xyFromCell(r, cells))
-coordinates(ctr)= ~x+y
-crs(ctr)=crs(compr)
-
-Vcompr <- extract(compr, ctr)
-
-NAT_DIS_NNI_POP_COMP <- cbind(NAT_DIS_NNI_POP, comp =Vcompr)
-NAT_DIS_NNI_POP_COMP <- NAT_DIS_NNI_POP_COMP %>% drop_na() 
-NAT_DIS_NNI_POP_COMP <- NAT_DIS_NNI_POP_COMP[,-5]
-colnames(NAT_DIS_NNI_POP_COMP) <- c("NNI","Road distance","Plots in Nature2000", "Population density", "Completeness") 
-
-install.packages("ggcorrplot")
-library(ggcorrplot)
-IDcor <- round(cor(NAT_DIS_NNI_POP_COMP),4)
-p1 <- ggcorrplot(IDcor, type = "upper",  lab = T, lab_col = "white", lab_size = 6, outline.color = "white", title = "Pearson's correlation - variables per grid", colors = c("#440154", "#21918C", "#FDE725"))+ ggplot2::labs(fill= "Correlation", x= c("NNI", "Road distance", "Plots in Nature2000", "Population density"), y=c("Road distance", "Plots in Nature2000", "Population density", "Completeness"))
-
-ggsave(plot = p1,
-       filename = "corr.jpg",
-       width = 8,
-       height = 8,
-       dpi = 600)
 
 
 ########## Tjostheim’s coefficient ###########
