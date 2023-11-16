5d4878c9217031c2504c702db903b4fb2e6f2d47
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2023-11-16 16:01:34 +0100

    Update 01_prepare_d.R

--- 01_prepare_d.R
+++ 01_prepare_d.R
@@ -25,20 +25,22 @@ d <- d %>% filter(Year >= 1992)
 
 # adding id values of grids with 0.5 degree of spatial resolution
 
-x <- raster()
-e <- extent( -180, 180, -90, 90)
-r<-raster::crop(x, e)
-r <- disaggregate(r, fact=2)
-vals <- 1:ncell(r)
-r <- setValues(r, vals)
-r
-r.sp <- as(r, "SpatialPolygonsDataFrame")
-crs(r.sp) <- "+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs "
+Europe <- ne_countries(continent = "Europe", returnclass = "sp")
+bbox <- st_bbox(Europe)
+polygon <- st_as_sfc(bbox)
+
+#hexabin
+size<- 0.5
+
+
+polygon.sp<- as(polygon, "Spatial")
+hex_points <- spsample(polygon.sp, type = "hexagonal", cellsize= size)
+hex_grid <- HexPoints2SpatialPolygons(hex_points, dx = size)
 
 dCoord <- d%>% dplyr::select(Longitude, Latitude, PlotObservationID)
 coordinates(dCoord)= ~Longitude+Latitude
-crs(dCoord)<-  "+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs "
-OV<- over(dCoord, r.sp)
+crs(dCoord)<-  "+proj=longlat +datum=WGS84 +no_defs "
+OV<- as.data.frame(over(dCoord, hex_grid))
 colnames(OV) <- "id"
 d<- cbind(OV, d)
 
